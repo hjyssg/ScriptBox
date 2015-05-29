da2dc45955b1b9bf7c7834af85e9a5f38703c942
Junyang Huang <huangjunyang47@gmail.com>
2015-05-30 00:45:26 +0800

    add a template

---
+++ templateScript.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs 
+import re
+import subprocess
+
+
+output = subprocess.check_output(["ls", "-R", "pixiv"])
+lines = output.split("\n")
+
+# lines = f.readlines()
+# f= codecs.open("p1.txt", mode = "r", encoding = "utf8")
+
+p = re.compile("(\d{3,})")
+
+for line in lines:
+  # print line
+
+  r1 = p.search(line)
+  if r1:
+    pass
+
